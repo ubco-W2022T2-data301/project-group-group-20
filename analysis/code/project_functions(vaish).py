5f16ac6529e1c8046c29411ff938318be6725851
vaishali <vaishaliraju6@gmail.com>
2023-03-27 16:11:58 -0700

    task 3 done

--- analysis/code/project_functions(vaish).py
+++ analysis/code/project_functions(vaish).py
@@ -1,6 +1,15 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib
+import matplotlib.pyplot as plt
+import project functions
+from .. import project_functions
+
 def load_and_process(path_to_csv_file):
 
    df1 = (pd.read_csv('BankChurners.csv'))\
+             .drop(columns = ['Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_1','Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_2'])\
              .drop(columns = ['Avg_Utilization_Ratio','Avg_Open_To_Buy', 'Total_Trans_Ct', 'Total_Amt_Chng_Q4_Q1','Total_Ct_Chng_Q4_Q1','Total_Trans_Amt'])\
              .dropna(subset = ['Months_on_book','Total_Revolving_Bal'])\
              .reset_index(drop=True)\
@@ -14,3 +23,6 @@ def load_and_process(path_to_csv_file):
    )
        
    return df2
+
+df = project_functions.load_and_process(url_or_path_to_csv_file)
+df
